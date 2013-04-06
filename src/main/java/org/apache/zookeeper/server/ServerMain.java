8d4c41707cf98e07d90b3a4e20eeca22c5949005
Lisa Glendenning <lisa@glendenning.net>
2013-04-05 20:07:40 -0700

    addendum to 7b9e5b8

--- src/main/java/org/apache/zookeeper/server/ServerMain.java
+++ src/main/java/org/apache/zookeeper/server/ServerMain.java
@@ -1,33 +1,10 @@
 package org.apache.zookeeper.server;
 
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.zookeeper.RequestExecutorService;
 import org.apache.zookeeper.ServiceMain;
 import org.apache.zookeeper.Zxid;
-import org.apache.zookeeper.util.Application;
-import org.apache.zookeeper.util.ApplicationService;
-import org.apache.zookeeper.util.Arguments;
-import org.apache.zookeeper.util.Eventful;
-import org.apache.zookeeper.util.EventfulEventBus;
 import org.apache.zookeeper.util.ServiceMonitor;
-import org.apache.zookeeper.util.VerboseThreadFactory;
-
-import com.google.common.collect.Lists;
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.Service;
-import com.google.inject.Injector;
-import com.google.inject.Module;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 
