e231bc266d02e60f5f3a55978c406bc7b33fdc53
Lisa Glendenning <lisa@glendenning.net>
2013-08-21 19:06:24 -0700

    simplified

--- src/main/java/edu/uw/zookeeper/client/LatencyMeasuringClient.java
+++ src/main/java/edu/uw/zookeeper/client/LatencyMeasuringClient.java
@@ -2,18 +2,15 @@ package edu.uw.zookeeper.client;
 
 import java.util.concurrent.Executor;
 
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 
-import edu.uw.zookeeper.common.ForwardingPromise;
 import edu.uw.zookeeper.common.Promise;
 import edu.uw.zookeeper.common.Publisher;
 import edu.uw.zookeeper.common.SettableFuturePromise;
 import edu.uw.zookeeper.protocol.Operation;
 
-public class LatencyMeasuringClient<I extends Operation.Request, O extends Operation.ProtocolResponse<?>> implements ClientExecutor<I,O>, FutureCallback<LatencyMeasuringClient.Measurement<I,O>> {
+public class LatencyMeasuringClient<I extends Operation.Request, O extends Operation.ProtocolResponse<?>> implements ClientExecutor<I,O> {
 
     public static <I extends Operation.Request, O extends Operation.ProtocolResponse<?>> LatencyMeasuringClient<I,O> create(
             ClientExecutor<? super I, O> delegate, Publisher publisher) {
@@ -41,12 +38,10 @@ public class LatencyMeasuringClient<I extends Operation.Request, O extends Opera
     public ListenableFuture<O> submit(I request, Promise<O> promise) {
         long start = System.nanoTime();
         ListenableFuture<O> future = delegate.submit(request, promise);
-        Listener<I,O> listener = Listener.create(start, request, future, SettableFuturePromise.<Measurement<I,O>>create());
-        Futures.addCallback(listener, this, executor);
+        Listener listener = new Listener(start, request, future);
         future.addListener(listener, executor);
         return future;
     }
-
 
     @Override
     public void register(Object handler) {
@@ -60,15 +55,6 @@ public class LatencyMeasuringClient<I extends Operation.Request, O extends Opera
         try {
             publisher.unregister(handler);
         } catch (IllegalArgumentException e) {}
-    }
-    
-    @Override
-    public void onSuccess(Measurement<I, O> result) {
-        publisher.post(result);
-    }
-
-    @Override
-    public void onFailure(Throwable t) {
     }
 
     public static class Measurement<I extends Operation.Request, O extends Operation.ProtocolResponse<?>> {
@@ -102,39 +88,19 @@ public class LatencyMeasuringClient<I extends Operation.Request, O extends Opera
         }
     }
 
-    public static class Listener<I extends Operation.Request, O extends Operation.ProtocolResponse<?>> extends ForwardingPromise<Measurement<I,O>> implements Runnable {
-        
-        public static <I extends Operation.Request, O extends Operation.ProtocolResponse<?>> Listener<I,O> create(
-                long start, 
-                I request, 
-                ListenableFuture<O> future,
-                Promise<Measurement<I, O>> promise) {
-            return new Listener<I,O>(start, request, future, promise);
-        }
-        
+    public class Listener implements Runnable {
+
         private final long start;
         private final I request;
         private final ListenableFuture<O> future;
-        private final Promise<Measurement<I, O>> promise;
         
         public Listener(
                 long start, 
                 I request, 
-                ListenableFuture<O> future,
-                Promise<Measurement<I, O>> promise) {
+                ListenableFuture<O> future) {
             this.start = start;
             this.request = request;
             this.future = future;
-            this.promise = promise;
-        }
-        
-        @Override
-        public boolean cancel(boolean mayInterruptIfRunning) {
-            boolean cancel = super.cancel(mayInterruptIfRunning);
-            if (cancel) {
-                future.cancel(mayInterruptIfRunning);
-            }
-            return cancel;
         }
 
         @Override
@@ -143,14 +109,9 @@ public class LatencyMeasuringClient<I extends Operation.Request, O extends Opera
             if (future.isDone() && ! future.isCancelled()) {
                 try {
                     O response = future.get();
-                    set(Measurement.create(nanos, request, response));
+                    publisher.post(Measurement.create(nanos, request, response));
                 } catch (Throwable t) {}
             }
-        }
-
-        @Override
-        protected Promise<Measurement<I,O>> delegate() {
-            return promise;
         }
     }
 }
