0fd4cfe372d92fda4c18b924de0bc7253685facd
Lisa Glendenning <lisa@glendenning.net>
2014-10-20 11:31:06 -0700

    minor

--- zkclient/src/main/java/edu/uw/zookeeper/client/SubmittedRequest.java
+++ zkclient/src/main/java/edu/uw/zookeeper/client/SubmittedRequest.java
@@ -2,14 +2,14 @@ package edu.uw.zookeeper.client;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-import edu.uw.zookeeper.client.ClientExecutor;
+import edu.uw.zookeeper.common.TaskExecutor;
 import edu.uw.zookeeper.common.ValueFuture;
 import edu.uw.zookeeper.protocol.Operation;
 
 public final class SubmittedRequest<I extends Operation.Request, O extends Operation.ProtocolResponse<?>> extends ValueFuture<I,O,ListenableFuture<O>> {
 
     public static <I extends Operation.Request, O extends Operation.ProtocolResponse<?>> SubmittedRequest<I,O> submit(
-            ClientExecutor<? super I,O,?> client, I request) {
+            TaskExecutor<? super I,O> client, I request) {
         return new SubmittedRequest<I,O>(request, client.submit(request));
     }
     
--- zkclient/src/main/java/edu/uw/zookeeper/client/SubmittedRequests.java
+++ zkclient/src/main/java/edu/uw/zookeeper/client/SubmittedRequests.java
@@ -6,17 +6,17 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
-import edu.uw.zookeeper.client.ClientExecutor;
+import edu.uw.zookeeper.common.TaskExecutor;
 import edu.uw.zookeeper.common.ValueFuture;
 import edu.uw.zookeeper.protocol.Operation;
 
 public final class SubmittedRequests<I extends Operation.Request, O extends Operation.ProtocolResponse<?>> extends ValueFuture<ImmutableList<I>,List<O>,ListenableFuture<List<O>>> {
 
-    public static <I extends Operation.Request, O extends Operation.ProtocolResponse<?>> SubmittedRequests<I,O> submitRequests(ClientExecutor<? super I,O,?> client, I... requests) {
+    public static <I extends Operation.Request, O extends Operation.ProtocolResponse<?>> SubmittedRequests<I,O> submitRequests(TaskExecutor<? super I,O> client, I... requests) {
         return submit(client, ImmutableList.copyOf(requests));
     }
     
-    public static <I extends Operation.Request, O extends Operation.ProtocolResponse<?>> SubmittedRequests<I,O> submit(ClientExecutor<? super I,O,?> client, Iterable<? extends I> requests) {
+    public static <I extends Operation.Request, O extends Operation.ProtocolResponse<?>> SubmittedRequests<I,O> submit(TaskExecutor<? super I,O> client, Iterable<? extends I> requests) {
         final ImmutableList.Builder<ListenableFuture<O>> futures = ImmutableList.builder();
         for (I request: requests) {
             futures.add(client.submit(request));
